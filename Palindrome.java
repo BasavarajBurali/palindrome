8d5c8a19c2ce361372dd11fd8e71bd3a8b3a8d2a
BasavarajBurali <134991758+BasavarajBurali@users.noreply.github.com>
2023-06-02 11:40:40 +0530

    Create Palindrome.java

---
+++ Palindrome.java
@@ -0,0 +1,23 @@
+package collectionProgram;
+
+public class Pelindrome {
+
+	public static void main(String[] args) {
+		int num=122;
+		int org_num=num;
+		int rev=0;
+		while(num>0) {
+			rev=rev*10+num%10;
+			num=num/10;
+			
+			
+		}
+		if(org_num==rev) {
+			System.out.println("its pelindrome "+rev);
+		}else {
+			System.out.println("its not pelindrome "+rev);
+		}
+
+	}
+
+}
